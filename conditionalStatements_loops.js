c4955ca6830e5cb5635a50479c33ff391b20cd92
0202je <0202jerm@gmail.com>
2026-05-27 04:26:30 +0200

    Three javascript task files

---
+++ conditionalStatements_loops.js
@@ -0,0 +1,5 @@
+let num1 = 23;
+let num2 = 5;
+// addition of the arithmetic operators
+let sum = num1 + num2;
+console.log(sum);
---
+++ datatypes_operators.js
@@ -0,0 +1,6 @@
+let favoriteColour = "blue";
+console.log(favoriteColour);
+
+//reassigning the variable
+favoriteColour = "red";
+console.log(favoriteColour);
